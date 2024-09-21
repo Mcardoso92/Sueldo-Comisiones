59a02ff8d13e3b60996d5aaa7f02d9d7815feae4
Mariano Cardoso <marianocardoso92@gmail.com>
2024-09-21 00:35:20 -0300

    Primera Subida

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EJERCICIO_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CALCULAR SUELDO MAS COMISIONES
+            //PASO 0: DECLARAR VARIABLES
+            float FACT, COM, SF, ST;
+
+            //PASO 1: INGRESAR DATOS
+            Console.WriteLine("Ingrese sueldo fijo actual: ");
+            SF = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese facturacion total: ");
+            FACT = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese comision: ");
+            COM = int.Parse(Console.ReadLine());
+
+            //PASO 2: CALCULAR
+            ST = SF + ((FACT * COM) / 100);            
+
+            //PASO 3: MOSTRAR SUELDO A COBRAR
+            Console.WriteLine("Sueldo a cobrar: " + ST);
+        }
+    }
+}
